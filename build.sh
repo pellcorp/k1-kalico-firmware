89c9e3e93d6a672fad2d44e6d4f04dc00863ba73
Jason Pell <jason@pellcorp.com>
2026-04-15 20:50:08 +1000

    restore og simple af firmware file naming and versioning

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# in case build is executed from outside current dir be a gem and change the dir
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
+cd $SCRIPT_DIR
+
+rm -rf outfw/
 ./_build.sh bed
 ./_build.sh noz
 ./_build.sh mcu
